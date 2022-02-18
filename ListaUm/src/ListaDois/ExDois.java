0f50eef786e8409def2deb19eec8a539e12f6dc2
Ana Vitória Göcthel <anagocthel@gmail.com>
2022-02-18 14:27:36 -0300

    feat: exercício 2 corrigido, adicionei o laço de repetição

--- ListaUm/src/ListaDois/ExDois.java
+++ ListaUm/src/ListaDois/ExDois.java
@@ -9,19 +9,21 @@ public class ExDois {
 
         System.out.println("Jogador 1 informe o número escolhido:");
             num1=scanner.nextInt();
-        System.out.println("Jogador 2 informe o número que você acha que o jogador 1 escolheu:");
-            num2= scanner.nextInt();
 
-        if (num1!=num2){
+
+        do {
+            System.out.println("Jogador 2 informe o número que você acha que o jogador 1 escolheu:");
+            num2= scanner.nextInt();
             if(num1>num2){
                 System.out.println("O número a ser encontrado é maior do que o que você digitou.");
             }
             else if(num1<num2){
                 System.out.println("O número a ser encontrado é menor do que o que você digitou.");
             }
-        } else {
-            System.out.println("Parabéns, você acertou!");
         }
+        while(num1!=num2);
+
+        System.out.println("Parabéns, você acertou!");
 
     }
 }
