993cb29b98d963ae1b1452c605de81f786f3ee41
Ana Vitória Göcthel <anagocthel@gmail.com>
2022-03-22 21:36:29 -0300

    HomeWork Exercício Query

--- modulo3_2/api-pessoa-gocthel/src/main/java/com/vemser/PrimeiroProjetoSpring/controller/PessoaController.java
+++ modulo3_2/api-pessoa-gocthel/src/main/java/com/vemser/PrimeiroProjetoSpring/controller/PessoaController.java
@@ -2,17 +2,22 @@ package com.vemser.PrimeiroProjetoSpring.controller;
 
 import com.vemser.PrimeiroProjetoSpring.dto.PessoaCreateDTO;
 import com.vemser.PrimeiroProjetoSpring.dto.PessoaDTO;
+import com.vemser.PrimeiroProjetoSpring.entity.PessoaEntity;
+import com.vemser.PrimeiroProjetoSpring.repository.PessoaRepository;
 import com.vemser.PrimeiroProjetoSpring.service.PessoaService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoa") // localhost:8080/pessoa
@@ -21,6 +26,8 @@ public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
+    @Autowired
+    private PessoaRepository pessoaRepository;
     @GetMapping("/hello") // localhost:8080/pessoa/hello
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello world!");
@@ -43,6 +50,19 @@ public class PessoaController {
     public ResponseEntity<List<PessoaDTO>> list() throws Exception {
         return ResponseEntity.ok(pessoaService.list());
     }
+    @GetMapping("/list_by_name") // localhost:8080/pessoa
+    public ResponseEntity<List<PessoaEntity>> findByName(@RequestParam String nome) throws Exception {
+        return ResponseEntity.ok(pessoaRepository.findByNomeContainingIgnoreCase(nome));
+    }
+
+    @GetMapping("/find_CPF")
+    public ResponseEntity<PessoaEntity> findByCPF(@RequestParam String cpf) throws Exception {
+        return ResponseEntity.ok(pessoaRepository.findByCpf(cpf));
+    }
+    @GetMapping("/find_By_Data_Nascimento")
+    public ResponseEntity<List<PessoaEntity>> findByDataNascimentoBetween(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  LocalDate dataInicio, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim) throws Exception {
+        return ResponseEntity.ok(pessoaRepository.findByDataNascimentoBetween(dataInicio, dataFim));
+    }
 
     @GetMapping("/{idPessoa}")
     public ResponseEntity<PessoaDTO> getPessoaById(@PathVariable("idPessoa") Integer id) throws Exception {
@@ -60,7 +80,5 @@ public class PessoaController {
         PessoaDTO pessoaDTO = pessoaService.delete(id);
         return ResponseEntity.ok("");
     }
-
-    L
 }
 
--- modulo3_2/api-pessoa-gocthel/src/main/java/com/vemser/PrimeiroProjetoSpring/repository/PessoaRepository.java
+++ modulo3_2/api-pessoa-gocthel/src/main/java/com/vemser/PrimeiroProjetoSpring/repository/PessoaRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<PessoaEntity, Integer> {
-    Optional<PessoaEntity> findByNomeContainingIgnoreCase(String nome);
-    Optional<PessoaEntity> findByCpf(String cpf);
-    Optional<PessoaEntity> findByDataNascimentoBetween(LocalDate dataInicio, LocalDate dataFim);
+    List<PessoaEntity> findByNomeContainingIgnoreCase(String nome);
+    PessoaEntity findByCpf(String cpf);
+    List<PessoaEntity> findByDataNascimentoBetween(LocalDate dataInicio, LocalDate dataFim);
 }
